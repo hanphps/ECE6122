d10f2ac33a8590fb0e3fa898ac63dfb0e930d478
Hannah <19543022+hanphps@users.noreply.github.com>
2021-09-10 13:25:12 -0400

    Add files via upload

---
+++ Lab0/hello.cpp
@@ -0,0 +1,28 @@
+/*
+Author: Hannah Phillips
+Class: ECE 6122
+Last Date Modified: 2021-08-31 07:02:45
+
+Description:
+I, Hannah Phillips , have successfully accessed my pace-ice account. I am
+able to transfer files from my pace-ice account to my local machine using an
+ftp client. I am ready for the next assignment on pace-ice.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+/*
+Function:
+  Main function to perform cout of required returns
+*/
+int main(){
+    cout << "My name is: Hannah Phillips" << endl;
+    cout << "This (\") is a double quote." << endl;
+    cout << "This (\') is a single quote." << endl;
+    cout << "This (\\) is a backslash." << endl;
+    cout << "This (/) is a forward slash." << endl;
+
+    return 0;
+}
